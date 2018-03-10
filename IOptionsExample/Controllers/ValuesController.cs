0540117881c4ed752591c02241f88dcd5e285a35
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2018-03-10 16:09:25 +0530

    Initial Commit

---
+++ IOptionsExample/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace IOptionsExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller    
+    {
+        public IOptions<MyConfigSettings> MyConfigSettings { get; }
+
+        public ValuesController(IOptions<MyConfigSettings> myConfigSettings)
+        {
+            MyConfigSettings = myConfigSettings;
+        }
+
+       
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string firstSetting = MyConfigSettings.Value.FirstSetting;
+            string secondSetting = MyConfigSettings.Value.SecondSetting;
+            return new string[] { firstSetting, secondSetting };
+        }        
+    }
+}
---
+++ IOptionsExample/MyConfigSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOptionsExample
+{
+    public class MyConfigSettings
+    {
+        public string FirstSetting { get; set; }
+        public string SecondSetting { get; set; }
+    }
+}
